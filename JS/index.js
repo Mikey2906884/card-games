9a3b802abb94abea9ec08d587ed40e9b632be4b9
Mikey2906884 <michael.sewell2906@outlook.com>
2025-03-25 02:21:33 -0500

    Started coding Go Fish and transferred card generator code to go fish js file. Updated card folder to better implement future back options. Added go fish animation css file.

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,17 @@
+import { goFish } from "./go-fish.js";
+
 function randomChoice(array) {
   const randomIndex = Math.floor(Math.random() * array.length);
   return array[randomIndex];
 }
+
+Array.prototype.shuffle = function () {
+  for (let i = this.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [this[i], this[j]] = [this[j], this[i]];
+  }
+  return this;
+};
 
 const values = [
   "A",
@@ -19,7 +29,7 @@ const values = [
   "K",
 ];
 const suites = ["♠", "♦", "♥", "♣"];
-const backColor = randomChoice(["BBACK", "RBACK"]);
+const backColor = randomChoice(["BLUE", "RED"]);
 const deck = [];
 const deckWithJokers = [];
 
@@ -29,35 +39,9 @@ for (let i of values) {
     deckWithJokers.push(i + j);
   }
 }
-
-Array.prototype.shuffle = function () {
-  for (let i = this.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [this[i], this[j]] = [this[j], this[i]];
-  }
-  return this;
-};
+deckWithJokers.push("BJOKER", "RJOKER");
 
 deck.shuffle();
+deckWithJokers.shuffle();
 
-for (let i of deck) {
-  const parent = document.getElementById("deck");
-  const container = document.createElement("div");
-  const card = document.createElement("div");
-  const front = document.createElement("div");
-  const back = document.createElement("div");
-
-  container.id = `${i}-container`;
-  container.className = "card-container";
-  card.id = i;
-  card.className = "card";
-  front.id = "front";
-  front.style.backgroundImage = `url("../Cards/${i}.png")`;
-  back.id = "back";
-  back.style.backgroundImage = `url("../Cards/${backColor}.png")`;
-
-  parent.appendChild(container);
-  container.appendChild(card);
-  card.appendChild(front);
-  card.appendChild(back);
-}
+goFish(deck, backColor);
